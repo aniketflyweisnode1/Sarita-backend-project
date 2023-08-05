d0e7a16d11269bc5198247c9c4bc23070448d5ee
Deepak Singh <deepakflyweis@gmail.com>
2023-08-05 14:51:06 +0530

    first commit

--- models/shippingOrder.js
+++ models/shippingOrder.js
@@ -1,138 +1,138 @@
-const mongoose = require("mongoose");
-const shippingOrderSchema = new mongoose.Schema(
-    {
-        // shippingCategory: {
-        //   type: String,
-        //   //required: true,
-        //   enum: ["Document", "Package"],
-        // },
-        // shippingFrom: {
-        //   type: mongoose.SchemaTypes.ObjectId,
-        //   ref: "Address",
-        // },
-        // shippingTo: {
-        //   type: mongoose.SchemaTypes.ObjectId,
-        //   ref: "Address",
-        // },
-        userId: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        shippingFrom: {
-            country: {
-                type: String,
-                required: true,
-            },
-            pinCode: {
-                type: Number,
-                required: true,
-            },
-            state: {
-                type: String,
-                required: true,
-            },
-            city: {
-                type: String,
-                required: true,
-            },
-        },
-        shippingTo: {
-            country: {
-                type: String,
-                required: true,
-            },
-            pinCode: {
-                type: Number,
-                required: true,
-            },
-            state: {
-                type: String,
-                required: true,
-            },
-            city: {
-                type: String,
-                required: true,
-            },
-        },
-        toMe: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "User",
-        },
+// const mongoose = require("mongoose");
+// const shippingOrderSchema = new mongoose.Schema(
+//     {
+//         // shippingCategory: {
+//         //   type: String,
+//         //   //required: true,
+//         //   enum: ["Document", "Package"],
+//         // },
+//         // shippingFrom: {
+//         //   type: mongoose.SchemaTypes.ObjectId,
+//         //   ref: "Address",
+//         // },
+//         // shippingTo: {
+//         //   type: mongoose.SchemaTypes.ObjectId,
+//         //   ref: "Address",
+//         // },
+//         userId: {
+//             type: mongoose.SchemaTypes.ObjectId,
+//             ref: "User",
+//             required: true,
+//         },
+//         shippingFrom: {
+//             country: {
+//                 type: String,
+//                 required: true,
+//             },
+//             pinCode: {
+//                 type: Number,
+//                 required: true,
+//             },
+//             state: {
+//                 type: String,
+//                 required: true,
+//             },
+//             city: {
+//                 type: String,
+//                 required: true,
+//             },
+//         },
+//         shippingTo: {
+//             country: {
+//                 type: String,
+//                 required: true,
+//             },
+//             pinCode: {
+//                 type: Number,
+//                 required: true,
+//             },
+//             state: {
+//                 type: String,
+//                 required: true,
+//             },
+//             city: {
+//                 type: String,
+//                 required: true,
+//             },
+//         },
+//         toMe: {
+//             type: mongoose.SchemaTypes.ObjectId,
+//             ref: "User",
+//         },
 
-        packages: [
-            {
-                packageCategory: {
-                    type: String,
+//         packages: [
+//             {
+//                 packageCategory: {
+//                     type: String,
 
-                    enum: ["Package", "Document"],
-                },
-                length: {
-                    type: Number,
-                    // required:true
-                },
-                unitLength: {
-                    type: String,
-                    enum: ["cm", "inches"],
-                    default: "cm",
-                },
-                width: {
-                    type: Number,
-                    // required:true
-                },
-                unitWidth: {
-                    type: String,
-                    enum: ["cm", "inches"],
-                    default: "cm",
-                },
+//                     enum: ["Package", "Document"],
+//                 },
+//                 length: {
+//                     type: Number,
+//                     // required:true
+//                 },
+//                 unitLength: {
+//                     type: String,
+//                     enum: ["cm", "inches"],
+//                     default: "cm",
+//                 },
+//                 width: {
+//                     type: Number,
+//                     // required:true
+//                 },
+//                 unitWidth: {
+//                     type: String,
+//                     enum: ["cm", "inches"],
+//                     default: "cm",
+//                 },
 
-                height: {
-                    type: Number,
-                    // required:true
-                },
-                unitHeight: {
-                    type: String,
-                    enum: ["cm", "inches"],
-                    default: "cm",
-                },
+//                 height: {
+//                     type: Number,
+//                     // required:true
+//                 },
+//                 unitHeight: {
+//                     type: String,
+//                     enum: ["cm", "inches"],
+//                     default: "cm",
+//                 },
 
-                quantity: {
-                    type: Number,
-                    // required:true
-                },
-                weight: {
-                    type: Number,
-                    // required:true
-                },
-                unitWeight: {
-                    type: String,
-                    enum: ["pounds", "lbs"],
-                    default: "lbs",
-                },
-            },
-        ],
-        shipmentValue: {
-            type: Number,
-            // required:true
-        },
-        currency: {
-            type: String,
-            // required:true
-        },
-        // amount: {
-        //   type: Number,
-        // },
-        pickupMan: {
-            type: String,
-            default: "not assigned",
-        },
-        parcelStatus: {
-            type: String,
-            default: "Pending",
-            enum: ["Approved", "Pending"],
-        },
-    },
-    { timestamps: true }
-);
+//                 quantity: {
+//                     type: Number,
+//                     // required:true
+//                 },
+//                 weight: {
+//                     type: Number,
+//                     // required:true
+//                 },
+//                 unitWeight: {
+//                     type: String,
+//                     enum: ["pounds", "lbs"],
+//                     default: "lbs",
+//                 },
+//             },
+//         ],
+//         shipmentValue: {
+//             type: Number,
+//             // required:true
+//         },
+//         currency: {
+//             type: String,
+//             // required:true
+//         },
+//         // amount: {
+//         //   type: Number,
+//         // },
+//         pickupMan: {
+//             type: String,
+//             default: "not assigned",
+//         },
+//         parcelStatus: {
+//             type: String,
+//             default: "Pending",
+//             enum: ["Approved", "Pending"],
+//         },
+//     },
+//     { timestamps: true }
+// );
 
-module.exports = mongoose.model("ShippingOrder", shippingOrderSchema);
+// module.exports = mongoose.model("ShippingOrder", shippingOrderSchema);
--- routes/shipping.routes.js
+++ routes/shipping.routes.js
@@ -1,66 +1,66 @@
-const shipping = require("../controllers/shippingOrder_controller");
-const { authJwt, validateShipping, objectId } = require("../middlewares");
+// const shipping = require("../controllers/shippingOrder_controller");
+// const { authJwt, validateShipping, objectId } = require("../middlewares");
 
-module.exports = (app) => {
-  app.post(
-    "/api/v1/shippings",
-    [authJwt.verifyToken, validateShipping.shippingBodies],
-    shipping.createShippingOrder
-  );
-  app.get(
-    "/api/v1/shippings",
-    [authJwt.verifyToken],
-    shipping.getShippingOrders
-  );
-  app.put("/api/v1/shippings/:id", shipping.updateShippingOrder);
-  app.delete(
-    "/api/v1/shippings/:id",
-    [authJwt.verifyToken],
-    shipping.deleteShippingOrder
-  );
-  app.get(
-    "/api/v1/shippings/:id",
-    [authJwt.verifyToken, objectId.validId],
-    shipping.getById
-  );
-  app.get(
-    "/api/v1/shippings/:userid ",
-    [authJwt.verifyToken],
-    shipping.getShippingOrdersByUser
-  );
+// module.exports = (app) => {
+//   app.post(
+//     "/api/v1/shippings",
+//     [authJwt.verifyToken, validateShipping.shippingBodies],
+//     shipping.createShippingOrder
+//   );
+//   app.get(
+//     "/api/v1/shippings",
+//     [authJwt.verifyToken],
+//     shipping.getShippingOrders
+//   );
+//   app.put("/api/v1/shippings/:id", shipping.updateShippingOrder);
+//   app.delete(
+//     "/api/v1/shippings/:id",
+//     [authJwt.verifyToken],
+//     shipping.deleteShippingOrder
+//   );
+//   app.get(
+//     "/api/v1/shippings/:id",
+//     [authJwt.verifyToken, objectId.validId],
+//     shipping.getById
+//   );
+//   app.get(
+//     "/api/v1/shippings/:userid ",
+//     [authJwt.verifyToken],
+//     shipping.getShippingOrdersByUser
+//   );
 
-  app.get(
-    "/api/v1/admin/shippings",
-    [authJwt.isAdmin],
-    shipping.getShippingOrders
-  );
-  app.put(
-    "/api/v1/admin/shippings/:id",
-    [authJwt.isAdmin, objectId.validId],
-    shipping.updateShippingOrder
-  );
-  app.delete(
-    "/api/v1/admin/shippings/:id",
-    [authJwt.isAdmin],
-    shipping.deleteShippingOrder
-  );
-  app.get(
-    "/api/v1/admin/shippings/:id",
-    [authJwt.isAdmin],
-    shipping.getShippingOrderById
-  );
+//   app.get(
+//     "/api/v1/admin/shippings",
+//     [authJwt.isAdmin],
+//     shipping.getShippingOrders
+//   );
+//   app.put(
+//     "/api/v1/admin/shippings/:id",
+//     [authJwt.isAdmin, objectId.validId],
+//     shipping.updateShippingOrder
+//   );
+//   app.delete(
+//     "/api/v1/admin/shippings/:id",
+//     [authJwt.isAdmin],
+//     shipping.deleteShippingOrder
+//   );
+//   app.get(
+//     "/api/v1/admin/shippings/:id",
+//     [authJwt.isAdmin],
+//     shipping.getShippingOrderById
+//   );
 
-  // tome orders
-  app.get(
-    "/api/v1/shippings/tome/:toMeId",
-    [authJwt.verifyToken],
-    shipping.getShippingOrder
-  );
+//   // tome orders
+//   app.get(
+//     "/api/v1/shippings/tome/:toMeId",
+//     [authJwt.verifyToken],
+//     shipping.getShippingOrder
+//   );
 
-  //pacakge updates of shipping orders
-  app.put(
-    "/api/v1/shippings/:id/packages/:packageId",
-    [authJwt.verifyToken, objectId.validId],
-    shipping.removePackage
-  );
-};
+//   //pacakge updates of shipping orders
+//   app.put(
+//     "/api/v1/shippings/:id/packages/:packageId",
+//     [authJwt.verifyToken, objectId.validId],
+//     shipping.removePackage
+//   );
+// };
